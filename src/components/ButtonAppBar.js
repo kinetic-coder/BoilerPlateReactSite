b4ac3d98a4f3b77c1763b23d4820f9fb27ed5b2b
Oliver Bullock <kinetic-coder@users.noreply.github.com>
2020-10-22 15:55:43 +0100

    Added blog and about buttons to main navigation bar

--- src/components/ButtonAppBar.js
+++ src/components/ButtonAppBar.js
@@ -7,6 +7,8 @@ import Button from "@material-ui/core/Button";
 import IconButton from "@material-ui/core/IconButton";
 import MenuIcon from "@material-ui/icons/Menu";
 
+import { useHistory } from "react-router-dom";
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1
@@ -21,6 +23,11 @@ const useStyles = makeStyles((theme) => ({
 
 export default function ButtonAppBar() {
   const classes = useStyles();
+  const history = useHistory();
+
+  const navigateTo = (newLocation) => {
+    console.log(`Navigating to ${newLocation}`);
+  };
 
   return (
     <div className={classes.root}>
@@ -37,7 +44,15 @@ export default function ButtonAppBar() {
           <Typography variant="h6" className={classes.title}>
             News
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Button color="inherit" onClick={() => navigateTo("/blog")}>
+            Blog
+          </Button>
+          <Button color="inherit" onClick={() => navigateTo("/about")}>
+            About
+          </Button>
+          <Button color="inherit" onClick={() => navigateTo("/login")}>
+            Login
+          </Button>
         </Toolbar>
       </AppBar>
     </div>
